c7b12a392b32a6968262754a7bbebee34ab50d4b
@Muhammad Touseef <immuhammadtouseef@gmail.com>
2025-05-01 11:47:55 +0500

    updated code

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const Parse = require('parse')
 
+Parse.initialize("mQv79SjdENHmpNINkTCkZ0HzwRaWJZiH8r71omaA", "mv0cZT96cfIPgoeoAIKkXutHg8ccFY26TWjeJZCD");
+Parse.serverURL = 'https://parseapi.back4app.com';
 app.get('/user' , (req , res )=>{
     res.send("we are connecting with server")
 })
